63e97deac0042d76fe939f580489cb470e51bb15
JCULukeM <luke.maclean@my.jcu.edu.au>
2017-10-27 15:59:04 +1000

    Add more practicals

--- Prac_05/wordOccurance.py
+++ Prac_05/wordOccurance.py
@@ -3,7 +3,18 @@ sentence = {}
 
 
 def main():
-    sentence = input("What is the sentence")
+    text_sentence = input("What is the sentence: ")
+    words = text_sentence.split()
+    for word in words:
+        frequency = sentence.get(word, 0)
+        sentence[word] = frequency + 1
+
+    words = list(sentence.keys())
+    words.sort()
+
+    max_length = max((len(word)for word in words))
+    for word in words:
+        print("{:{}} : {}".format(word, max_length, sentence[word]))
 
 
 main()
